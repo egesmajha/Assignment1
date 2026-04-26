432a5f2e36d149b2749a093ef9e7c12d1f08ba25
egesmajha <pamiregeyildiz@gmail.com>
2026-04-26 15:30:27 +0300

    Variables End

--- Source/BirdOfPrey/Private/BaseAIController.h
+++ Source/BirdOfPrey/Private/BaseAIController.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "BasePlayerController.h"
+#include "BaseGameAgent.h"
 #include "BaseAIController.generated.h"
 
 /**
@@ -13,5 +15,22 @@ UCLASS()
 class ABaseAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+private:
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	float LastFireTime = 0.0f;
+
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	bool bIsFiring = false;
+
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	float RefireDelay = 0.0f;
+
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	bool bShouldUpdateAim = false;
+
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	ABasePlayerController* Target;
+
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	ABaseGameAgent* ControlledAgent;
 };
--- Source/BirdOfPrey/Private/ProjectileWeapon.h
+++ Source/BirdOfPrey/Private/ProjectileWeapon.h
@@ -7,30 +7,33 @@
 #include "BaseProjectile.h"
 #include "ProjectileWeapon.generated.h"
 
+
+
 /**
- * 
+ *
  */
 UCLASS()
 class AProjectileWeapon : public ABaseWeapon
 {
 	GENERATED_BODY()
+
 private:
-	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey", meta = (AllowPrivateAccess = "true"))
 	TSubclassOf<ABaseProjectile> ProjectileType;
 
-	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey", meta = (AllowPrivateAccess = "true"))
 	float RefireTime = 0.0f;
 
-	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey", meta = (AllowPrivateAccess = "true"))
 	float LastFireTime = 0.0f;
 
-	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey", meta = (AllowPrivateAccess = "true"))
 	int32 NumShots = 0;
 
-	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey", meta = (AllowPrivateAccess = "true"))
 	bool bIsFiring = false;
 
-	UPROPERTY(EditAnywhere, Category = "BirdOfPrey")
+	UPROPERTY(EditAnywhere, Category = "BirdOfPrey", meta = (AllowPrivateAccess = "true"))
 	float DesiredZ = 0.0f;
-	
 };
